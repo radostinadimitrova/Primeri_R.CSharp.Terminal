f5d421dd187fce9193938194fbd594dc281cfdda
Радостина Димитрова <radostina.dimitrova.7@gmail.com>
2016-02-03 13:53:59 +0200

    Модул 02, видео 01 - Добавяне на коментари

---
+++ Modul_R_02/Modul_R_02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Modul_R_02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+	
+			//Дефиниране на променливи
+
+
+			//Събиране на числа
+
+
+
+			//писане в конзолата
+
+
+		}
+	}
+}
